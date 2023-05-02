5fd168ea97687c3faf27088af5130c62584731ad
yuanlee <yuanlee@163.com>
2023-05-02 16:21:04 +0800

    Done: 函数式组件

--- src/views/components/21-functional-component.vue
+++ src/views/components/21-functional-component.vue
@@ -1,14 +1,33 @@
 <script setup lang='ts'>
+import type {SetupContext} from "vue";
 
-import { ref } from "vue"
+import {ref, h} from "vue"
 
 /**
  * 实现该函数式组件 :
  * 1. 使用`list`数据渲染列表元素 (ul/li)
  * 2. 当点击列表子元素时,将其文本颜色更改为红色
  */
-const ListComponent = () => {
+type Item = {
+  name: string
 }
+type Props = {
+  list: Item[]
+  activeIndex: number
+  toggle: Function
+}
+const ListComponent = (props: Props, ctx: SetupContext) => {
+  console.log(props)
+  console.log(ctx)
+  return h('ol', props?.list.map((item, idx) => h('li', {
+    style: {
+      color: idx === props.activeIndex ? 'red' : ''
+    },
+    onclick: () => ctx.emit('toggle', idx)
+  }, item.name)))
+}
+ListComponent.props = ['list', 'activeIndex']
+ListComponent.emits = ['toggle']
 
 const list = [{
   name: "John",
@@ -21,6 +40,7 @@ const list = [{
 const activeIndex = ref(0)
 
 function toggle(index: number) {
+  console.log('toggle>index:', index)
   activeIndex.value = index
 }
 
