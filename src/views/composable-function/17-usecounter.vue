a4a223d526aa41aed37c4bc5fe7a6d1ce889bef4
yuanlee <yuanlee@163.com>
2023-05-02 18:20:52 +0800

    Done: 计数器

--- src/views/composable-function/17-usecounter.vue
+++ src/views/composable-function/17-usecounter.vue
@@ -5,8 +5,14 @@
     在这个挑战中，我们将实现一个计数器。 👇:
     <hr>
   </h2>
+  <pre>Count: {{ count }}</pre>
+  <button @click="inc">Inc</button>
+  <button @click="dec">Dec</button>
+  <button @click="reset">Reset</button>
 </template>
 <script setup lang='ts'>
+
+import {ref} from "vue";
 
 interface UseCounterOptions {
   min?: number
@@ -21,10 +27,31 @@ interface UseCounterOptions {
  * 4. 最小值 & 最大值 选项支持
  */
 function useCounter(initialValue = 0, options: UseCounterOptions = {}) {
+  const {min = -Infinity, max = Infinity} = options
+  const count = ref(init(Number(initialValue)))
 
+  function init(val: number): number {
+    return val < min ? min : val > max ? max : val
+  }
+
+  function inc() {
+    count.value = init(count.value + 1)
+  }
+
+  function dec() {
+    count.value = init(count.value - 1)
+  }
+
+  function reset() {
+    count.value = init(initialValue)
+  }
+
+  return {
+    count, inc, dec, reset
+  }
 }
 
-const { count, inc, dec, reset } = useCounter(0, { min: 0, max: 10 })
+const {count, inc, dec, reset} = useCounter(-1, {min: 0, max: 10})
 
 </script>
 
