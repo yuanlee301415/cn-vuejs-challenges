3ed7aae866128e837f71879f930d610820690745
yuanlee <yuanlee@163.com>
2023-05-02 20:35:20 +0800

    Done: 鼠标坐标

--- src/views/composable-function/25-usemouse.vue
+++ src/views/composable-function/25-usemouse.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
+import {onMounted, reactive, toRefs, onUnmounted} from "vue";
 
 // Implement ...
-function useEventListener(target, event, callback) {
-
+function useEventListener(target: EventTarget, event: keyof HTMLElementEventMap, callback: EventListener) {
+  onMounted(() => {
+    target.addEventListener(event, callback)
+  })
+  onUnmounted(() => {
+    target.removeEventListener(event, callback)
+  })
 }
 
 // Implement ...
+type MousePoint = {
+  x: number
+  y: number
+}
+
 function useMouse() {
-  useEventListener(window, "mousemove", () => {
+  const point = reactive<MousePoint>({
+    x: 0,
+    y: 0
   })
+  useEventListener(window, "mousemove", (evt) => {
+    console.log(evt)
+    const {x, y} = evt as MouseEvent
+    point.x = x
+    point.y = y
+  })
+  return toRefs(point)
 }
 
 const {x, y} = useMouse()
