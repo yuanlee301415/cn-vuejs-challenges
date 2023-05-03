1df687c2e963b214b4d5ff6bd56bd6c4ac13540a
yuanlee <yuanlee@163.com>
2023-05-03 17:56:18 +0800

    Done: Effect作用域 API

--- src/views/composition-api/8-effect-scope.vue
+++ src/views/composition-api/8-effect-scope.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import {ref, computed, watch, watchEffect} from "vue"
+import {ref, computed, watch, watchEffect, effectScope} from "vue"
 
 const counter = ref(1)
 const doubled = computed(() => counter.value * 2)
 
 // 使用 `effectScope` API 使这些Effect效果在触发一次后停止
-
-watch(doubled, () => console.log('watch:', doubled.value))
-watchEffect(() => console.log("watchEffect:", doubled.value))
+const scope = effectScope()
+scope.run(() => {
+  watch(doubled, () => {
+    console.log('watch:', doubled.value)
+    scope.stop()
+  })
+  watchEffect(() => console.log("watchEffect:", doubled.value))
+})
 
 counter.value = 2
 
